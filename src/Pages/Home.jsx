5c1e5a3445fe26a5da55e0f2b565abdd3ff9fa27
RNtaate <royhyde14@gmail.com>
2023-04-21 00:58:41 +0300

    Create What I do section
    Import and use it in the Home page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 
 import Header from './sections/HomePageSections/Header'
+import WhatIDo from './sections/HomePageSections/WhatIDo'
 
 export default function Home() {
   return (
     <>
       <Header />
+      <WhatIDo />
     </>
   )
 }
---
+++ src/Pages/sections/HomePageSections/WhatIDo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function WhatIDo() {
+  return (
+    <section className={`bg-secondary`}>
+      <h2>What I Do</h2>
+    </section>
+  )
+}
