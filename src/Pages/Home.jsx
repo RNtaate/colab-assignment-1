1bbd5bf1a53cdb4f2a94728ce0c574f3b83dbea9
RNtaate <royhyde14@gmail.com>
2023-04-21 05:18:07 +0300

    Create projects section
    Import and use it in the Home page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 
 import Header from './sections/HomePageSections/Header'
 import WhatIDo from './sections/HomePageSections/WhatIDo'
+import Projects from './sections/HomePageSections/Projects'
 
 export default function Home() {
   return (
     <>
       <Header />
       <WhatIDo />
+      <Projects />
     </>
   )
 }
