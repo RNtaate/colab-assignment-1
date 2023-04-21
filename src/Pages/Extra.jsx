95302ad7f79700d394c593614f8cd25eea427233
RNtaate <royhyde14@gmail.com>
2023-04-22 01:38:19 +0300

    Update the design of the Extra page
    Add the associated styling file.

--- src/Pages/Extra.jsx
+++ src/Pages/Extra.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
 
+import styles from '../styles/Extra.module.css'
+import aboutStyles from '../styles/About.module.css'
+import Joke from '../components/Joke'
+
 export default function Extra() {
   return (
-    <div>
-      <h1>The Extra Page</h1>
+    <div
+      className={`${styles.extraMain} bg-primary d-flex justify-content-center align-items-center`}
+    >
+      <div
+        className={`${styles.extraContainer} d-flex flex-col align-items-center justify-content-center`}
+      >
+        <h1 className={`${aboutStyles.aboutHeading} text-center`}>EXTRA</h1>
+
+        <h3 className={`text-center`}>
+          "Every body deserves a little laughter in their lives. Which is why a
+          random joke will show up on this page every time you visit it. 😊"
+        </h3>
+
+        <Joke />
+      </div>
     </div>
   )
 }
