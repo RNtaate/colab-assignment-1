d6f23515d04ca0157956741fab44131d676c7e1f
RNtaate <royhyde14@gmail.com>
2023-04-21 16:55:53 +0300

    Add reasponsiveness to the what I DO section

--- src/Pages/sections/HomePageSections/WhatIDo.jsx
+++ src/Pages/sections/HomePageSections/WhatIDo.jsx
@@ -4,6 +4,7 @@ import frontendImage from '../../../assets/frontend3.svg'
 import backendImage from '../../../assets/backend1.svg'
 import mobileImage from '../../../assets/mobile-dev.svg'
 import WhatIDoCard from '../../../components/WhatIDoCard'
+import whatIDoImage from '../../../assets/whatIDo.svg'
 import styles from '../../../styles/WhatIDo.module.css'
 import SkillsCarousel from '../../../components/SkillsCarousel'
 
@@ -13,27 +14,37 @@ export default function WhatIDo() {
   return (
     <section className={`bg-primary d-flex flex-col`}>
       <h2 className={`${styles.whatIDoHeading} text-center`}>WHAT I DO</h2>
+
       <div
-        className={`${styles.cardsContainerDiv} d-flex justify-content-center`}
+        className={`${styles.whatIDoContainer} d-flex align-items-center justify-content-center`}
       >
-        <WhatIDoCard
-          imgSrc={frontendImage}
-          size={svgDimensions}
-          title={'Frontend Development'}
-          corner={'borderTopRightRadius'}
-        />
-        <WhatIDoCard
-          imgSrc={backendImage}
-          size={svgDimensions}
-          title={'Backend Development'}
-          corner={'borderBottomRightRadius'}
-        />
-        <WhatIDoCard
-          imgSrc={mobileImage}
-          size={svgDimensions}
-          title={'Mobile Development'}
-          corner={'borderBottomLeftRadius'}
-        />
+        <div
+          className={`${styles.imageDiv} d-flex align-items-center justify-content-center`}
+        >
+          <img src={whatIDoImage} />
+        </div>
+        <div
+          className={`${styles.cardsContainerDiv} d-flex justify-content-center`}
+        >
+          <WhatIDoCard
+            imgSrc={frontendImage}
+            size={svgDimensions}
+            title={'Frontend Development'}
+            corner={'borderTopRightRadius'}
+          />
+          <WhatIDoCard
+            imgSrc={backendImage}
+            size={svgDimensions}
+            title={'Backend Development'}
+            corner={'borderBottomRightRadius'}
+          />
+          <WhatIDoCard
+            imgSrc={mobileImage}
+            size={svgDimensions}
+            title={'Mobile Development'}
+            corner={'borderBottomLeftRadius'}
+          />
+        </div>
       </div>
 
       <SkillsCarousel />
