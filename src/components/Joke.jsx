85753b6bdd3734efb6064cbf4fbad28088518f36
RNtaate <royhyde14@gmail.com>
2023-04-22 01:37:41 +0300

    Update the design and add styling to the joke component

--- src/components/Joke.jsx
+++ src/components/Joke.jsx
@@ -1,5 +1,24 @@
 import React from 'react'
 
+import styles from '../styles/Joke.module.css'
+
 export default function Joke() {
-  return <div>Joke</div>
+  return (
+    <div
+      className={`${styles.jokeContainer} d-flex justify-content-center align-items-center`}
+    >
+      <span className={styles.upperSpan}></span>
+      <span className={styles.lowerSpan}></span>
+
+      <div className={`${styles.jokeDetails} bg-secondary`}>
+        <p className={styles.punchLine}>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut ea
+          asperiores distinctio amet ipsam reprehenderit!
+        </p>
+        <p className={styles.pun}>
+          Lorem ipsum dolor sit, amet consectetur adipisicing.
+        </p>
+      </div>
+    </div>
+  )
 }
