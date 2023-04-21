27396d405b78c70908c0e8c490cd2ef6337ca154
RNtaate <royhyde14@gmail.com>
2023-04-21 14:10:27 +0300

    Import and use the contact form compoent in Contact secion
    Update styles to accomodate the contact form component

--- src/Pages/sections/HomePageSections/Contact.jsx
+++ src/Pages/sections/HomePageSections/Contact.jsx
@@ -1,5 +1,14 @@
 import React from 'react'
 
+import styles from '../../../styles/Contact.module.css'
+import ContactForm from '../../../components/ContactForm'
+
 export default function Contact() {
-  return <div>Contact</div>
+  return (
+    <section
+      className={`${styles.contactSection} bg-primary d-flex justify-content-center align-items-center`}
+    >
+      <ContactForm />
+    </section>
+  )
 }
