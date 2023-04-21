988f5a13244beb9158cb010b6d241330d91cface
RNtaate <royhyde14@gmail.com>
2023-04-21 03:02:47 +0300

    Import and use the WhatIDoCard component into WhatIDo section
    Create the styles for the WhatIDo section

--- src/Pages/sections/HomePageSections/WhatIDo.jsx
+++ src/Pages/sections/HomePageSections/WhatIDo.jsx
@@ -1,9 +1,38 @@
 import React from 'react'
 
+import frontendImage from '../../../assets/frontend3.svg'
+import backendImage from '../../../assets/backend1.svg'
+import mobileImage from '../../../assets/mobile-dev.svg'
+import WhatIDoCard from '../../../components/WhatIDoCard'
+import styles from '../../../styles/WhatIDo.module.css'
 export default function WhatIDo() {
+  const svgDimensions = 100
+
   return (
-    <section className={`bg-secondary`}>
-      <h2>What I Do</h2>
+    <section className={`bg-primary`}>
+      <h2 className={`${styles.whatIDoHeading} text-center`}>WHAT I DO</h2>
+      <div
+        className={`${styles.cardsContainerDiv} d-flex justify-content-center`}
+      >
+        <WhatIDoCard
+          imgSrc={frontendImage}
+          size={svgDimensions}
+          title={'Frontend Development'}
+          corner={'borderTopRightRadius'}
+        />
+        <WhatIDoCard
+          imgSrc={backendImage}
+          size={svgDimensions}
+          title={'Backend Development'}
+          corner={'borderBottomRightRadius'}
+        />
+        <WhatIDoCard
+          imgSrc={mobileImage}
+          size={svgDimensions}
+          title={'Mobile Development'}
+          corner={'borderBottomLeftRadius'}
+        />
+      </div>
     </section>
   )
 }
