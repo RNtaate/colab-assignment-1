afb2e3714e7a8f44a1e18e638b2d07fc8f4add40
RNtaate <royhyde14@gmail.com>
2023-04-21 08:34:20 +0300

    Import and use the ProjectCard component
    Add more styling to the Projects section to facilitate the usage of the ProjectCard.

--- src/Pages/sections/HomePageSections/Projects.jsx
+++ src/Pages/sections/HomePageSections/Projects.jsx
@@ -1,5 +1,23 @@
 import React from 'react'
 
+import ProjectCard from '../../../components/ProjectCard'
+import styles from '../../../styles/Projects.module.css'
+import runnerImage from '../../../assets/endless-runner.png'
+import norpgramImage from '../../../assets/norpgram.png'
+import norpmoviesImage from '../../../assets/norp_movies.png'
+import { projectsData } from '../../../services/projects'
+
 export default function Projects() {
-  return <div>Projects</div>
+  const projectImages = [runnerImage, norpgramImage, norpmoviesImage]
+
+  return (
+    <section className={`bg-secondary text-center`}>
+      <h2>PORTFOLIO</h2>
+      <div className={`${styles.projectsContainer}`}>
+        {projectsData.map((project) => {
+          return <ProjectCard key={project.title} project={project} />
+        })}
+      </div>
+    </section>
+  )
 }
