452852ff6a5ef2ae620e9a8d07ed843e9b1436f2
RNtaate <royhyde14@gmail.com>
2023-04-22 03:36:38 +0300

    Import and use the navbar in App container

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,19 @@ import './styles/App.css'
 import Home from './Pages/Home'
 import About from './Pages/About'
 import Extra from './Pages/Extra'
-import { Route, Routes } from 'react-router-dom'
+import NavBar from './components/NavBar'
+import { Route, Routes, Link } from 'react-router-dom'
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/extra" element={<Extra />} />
-    </Routes>
+    <main>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/extra" element={<Extra />} />
+      </Routes>
+    </main>
   )
 }
 
