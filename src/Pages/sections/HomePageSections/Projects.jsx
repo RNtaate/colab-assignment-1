e64a1ad8b0011fe9bae2308d09513950182eaf0b
RNtaate <royhyde14@gmail.com>
2023-04-22 16:38:31 +0300

    Remove unnecessary code

--- src/Pages/sections/HomePageSections/Projects.jsx
+++ src/Pages/sections/HomePageSections/Projects.jsx
@@ -2,14 +2,9 @@ import React from 'react'
 
 import ProjectCard from '../../../components/ProjectCard'
 import styles from '../../../styles/Projects.module.css'
-import runnerImage from '../../../assets/endless-runner.png'
-import norpgramImage from '../../../assets/norpgram.png'
-import norpmoviesImage from '../../../assets/norp_movies.png'
 import { projectsData } from '../../../services/projects'
 
 export default function Projects() {
-  const projectImages = [runnerImage, norpgramImage, norpmoviesImage]
-
   return (
     <section className={`bg-secondary text-center`}>
       <h2>PORTFOLIO</h2>
