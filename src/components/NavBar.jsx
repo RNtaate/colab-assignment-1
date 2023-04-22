c342ceeb0899cd5a27af66c668c2d12152b44d75
RNtaate <royhyde14@gmail.com>
2023-04-22 04:44:45 +0300

    Make Navbar responsive on multiple screens

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import styles from '../styles/NavBar.module.css'
+import logoImage from '../assets/logo.svg'
 export default function NavBar() {
   return (
-    <nav className={`${styles.navBar}`}>
+    <nav className={`${styles.navBar} d-flex align-items-center`}>
+      <div className={`${styles.navLogo}`}>
+        <Link to="/">
+          <div className={`${styles.logoDiv}`}>
+            <img src={logoImage} alt="Logo" />
+          </div>
+        </Link>
+      </div>
       <ul
         className={`${styles.navBarList} d-flex align-items-center justify-content-center`}
       >
