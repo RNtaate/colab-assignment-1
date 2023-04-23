2ba71134c3affb7a1d078900d3ba0dc5aafe2ff9
RNtaate <royhyde14@gmail.com>
2023-04-23 20:04:49 +0300

    Set up aos configuration for the entire project

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 import App from './App';
 import './styles/index.css';
+
+AOS.init();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
