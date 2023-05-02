48ac4f4ebf460a254e2604fe231bc97417eaa45a
RNtaate <royhyde14@gmail.com>
2023-05-02 20:08:50 +0300

    Use AnimatePresence from framer motion to facilitate switching of pages with animation
    Add styling to body for the animation flow.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useLocation } from 'react-router-dom';
+import { AnimatePresence } from 'framer-motion';
+
 import Home from './Pages/Home';
 import About from './Pages/About';
 import Extra from './Pages/Extra';
@@ -7,16 +9,27 @@ import NavBar from './components/NavBar';
 import ScrollToTop from './components/ScrollToTop';
 
 function App() {
+  const location = useLocation();
+
   return (
     <main>
       <NavBar />
-      <ScrollToTop>
-        <Routes>
+      <AnimatePresence
+        mode="wait"
+        onExitComplete={() => {
+          if (typeof window !== 'undefined') {
+            window.scrollTo({ top: 0 });
+          }
+        }}
+      >
+        {/* <ScrollToTop> */}
+        <Routes location={location} key={location.key}>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/extra" element={<Extra />} />
         </Routes>
-      </ScrollToTop>
+        {/* </ScrollToTop> */}
+      </AnimatePresence>
     </main>
   );
 }
