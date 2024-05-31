f93eb09accad81569201f3d1a86c72c8377c3529
RNtaate <royhyde14@gmail.com>
2024-05-31 18:23:35 +0300

    Add framer motion to the projects page
    Add a navigational button to the projects section of the home page to lead the user to the projects page.
    
    Add styling for the added button on the projects section of the home page.

--- src/Pages/ProjectsPage.jsx
+++ src/Pages/ProjectsPage.jsx
@@ -1,7 +1,37 @@
 import React from 'react';
+import { motion } from 'framer-motion';
 
 const ProjectsPage = () => {
-  return <div>ProjectsPage</div>;
+  const projectsPageVaraints = {
+    hidden: {
+      opacity: 0,
+    },
+    visible: {
+      opacity: 1,
+      transition: {
+        duration: 0.5,
+        ease: 'easeInOut',
+      },
+    },
+    exit: {
+      opacity: 0,
+      transition: {
+        duration: 0.5,
+        ease: 'easeInOut',
+      },
+    },
+  };
+
+  return (
+    <motion.div
+      variants={projectsPageVaraints}
+      initial="hidden"
+      animate="visible"
+      exit="exit"
+    >
+      <h1>Projects Page</h1>
+    </motion.div>
+  );
 };
 
 export default ProjectsPage;
--- src/Pages/sections/HomePageSections/Projects.jsx
+++ src/Pages/sections/HomePageSections/Projects.jsx
@@ -1,8 +1,10 @@
 import React, { forwardRef } from 'react';
+import { NavLink } from 'react-router-dom';
 
 import ProjectCard from '../../../components/ProjectCard';
 import styles from '../../../styles/Projects.module.css';
 import projectsData from '../../../services/projects';
+import formStyles from '../../../styles/ContactForm.module.css';
 
 const animationNamesArray = [
   'fade-left',
@@ -16,14 +18,27 @@ const animationNamesArray = [
 const Projects = (props, ref) => (
   <section className="bg-secondary text-center" ref={ref}>
     <h2 data-aos="fade-down">PORTFOLIO</h2>
-    <div className={`${styles.projectsContainer}`}>
-      {projectsData.map((project, index) => (
-        <ProjectCard
-          key={project.title}
-          project={project}
-          name={animationNamesArray[index % animationNamesArray.length]}
-        />
-      ))}
+    <div>
+      <div className={`${styles.projectsContainer}`}>
+        {projectsData.map((project, index) => (
+          <ProjectCard
+            key={project.title}
+            project={project}
+            name={animationNamesArray[index % animationNamesArray.length]}
+          />
+        ))}
+      </div>
+
+      <div className={`${styles.moreButtonDiv}`}>
+        <NavLink to="/projects">
+          <button
+            className={`${formStyles.contactFormSubmitButton} ${styles.moreButton}`}
+            type="button"
+          >
+            MORE PROJECTS
+          </button>
+        </NavLink>
+      </div>
     </div>
   </section>
 );
